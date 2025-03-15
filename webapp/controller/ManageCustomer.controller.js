61ac2f42915725195b0a6479edcdfe6ad4be799a
Chand <chandh@terralink-global.com>
2025-03-15 18:52:29 +0530

    customer page added

---
+++ webapp/controller/ManageCustomer.controller.js
@@ -0,0 +1,18 @@
+sap.ui.define([
+    "./BaseController",
+], function(BaseController) {
+	"use strict";
+
+    return BaseController.extend("micro.crm.frontend.controller.ManageCustomer", {
+
+	
+		onInit: function() {
+			
+		},
+
+		
+
+	});
+
+	
+});
